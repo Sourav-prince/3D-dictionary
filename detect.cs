507f5c17966fcf37b129872eb344a03a8d4148f4
sourav chatterjee <31128319+Sourav-prince@users.noreply.github.com>
2017-10-14 10:56:55 +0530

    add 2
    upload files

---
+++ detect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Vuforia
+{
+public class detect : MonoBehaviour, ITrackableEventHandler {
+	public Text word;
+	private TrackableBehaviour mTrackableBehaviour;
+	// Use this for initialization
+	void Start () {
+		word.text = "start";
+			mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+			if (mTrackableBehaviour)
+			{
+				mTrackableBehaviour.RegisterTrackableEventHandler(this);
+			}
+	}
+	
+		public void OnTrackableStateChanged(
+			TrackableBehaviour.Status previousStatus,
+			TrackableBehaviour.Status newStatus)
+		{
+			if (newStatus == TrackableBehaviour.Status.DETECTED ||
+				newStatus == TrackableBehaviour.Status.TRACKED ||
+				newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+			{
+				Found();
+			}
+			else
+			{
+				Lost();
+			}
+		}
+		private void Found()
+		{
+			word.text = mTrackableBehaviour.TrackableName;
+			Debug.Log ("my func" + mTrackableBehaviour.TrackableName);
+		}
+		private void Lost()
+		{
+			word.text ="";
+			Debug.Log ("my func no obj");
+		}
+
+}
+}
